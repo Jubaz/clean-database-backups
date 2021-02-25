05de384038b94b131673cb0ff63eac221aa0bf26
juba <abdelrahman.fawzy@areebgroup.com>
2021-02-25 16:06:22 +0200

    Test

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-// Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({ region: 'eu-west-2' });
+const AWS = require("aws-sdk");
+const MOMENT = require("moment");
+const numberOfDays = 15;
+
+AWS.config.update({ region: "eu-west-2" });
 
 // Create S3 service object
-s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 // Call S3 to list the buckets
 
-s3.listBuckets(function (err, data) {
-  if (!err) {
-    data.Buckets.forEach(element => {
-      if (element.Name.includes('database')) {
-        getToBeDeletedObjects(element.Name)
-      }
-    });
-  }
-});
+listBuckets();
 
+async function listBuckets() {
+  var hamada = await s3.listBuckets();
 
-function getToBeDeletedObjects(BucketName) {
-  var currentDate = new Date();
-  var yesterdayDate = currentDate.setDate(currentDate.getDate() - 15);
-  var sub = new Date(yesterdayDate);
+  console.log(hamada.then);
+}
 
+function listObjects(BucketName) {
   s3.listObjects({ Bucket: BucketName }, function (err, data) {
-    data.Contents.forEach(element => {
-      if (element.LastModified < sub && element.Key.includes('backups/ebdb')) {
+    data.Contents.forEach((element) => {
+      let daysDiff = MOMENT().diff(element.LastModified, "days");
+      if (daysDiff > numberOfDays && element.Key.includes("backups/ebdb")) {
         deleteObject(BucketName, element.Key);
       }
     });
   });
 }
 
-
 function deleteObject(bucketName, objectKey) {
-  console.log('Item ' + objectKey + ' from bucket ' + bucketName)
+  console.log("Item " + objectKey + " from bucket " + bucketName);
   /*
     s3.deleteObject({Bucket: bucketName,Key: objectKey}, function(err, data) {
       if(!err){
